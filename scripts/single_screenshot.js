e2978c8a5c7edebd4ce34fbad630ee07ee605f73
Roman Gorodeckij <holms@holms.lt>
2014-03-25 09:04:02 +0000

    Refactored single_screenshot.js

--- scripts/single_screenshot.js
+++ scripts/single_screenshot.js
@@ -1,31 +1,14 @@
-/*
-* Takes provided URL passed as argument and make screenshots of this page with several viewport sizes.
-* These viewport sizes are arbitrary, taken from iPhone & iPad specs, modify the array as needed
-* @author: https://gist.github.com/nhoizey/4060568
-* Modified By: cthorn@phase2technology.com
-* CHANGELOG: new viewports
-* Usage:
-* $ casperjs screenshots.js http://example.com
-*/
-
 var casper = require('casper').create({
   verbose: true,
   logLevel: "debug"
 });
 
-var screenshotUrl = 'http://google.com/',
-    screenshotNow = new Date(),
-    screenshotFileName = "thumbie.png"
-    viewports = [
-      {
-        'name': 'xvfb-1024-768',
-        'viewport': { width: 1024, height: 768 }
-      }
-    ];
+var screenshotUrl = 'http://google.com/'
+var screenshotFileName = "thumbie.png"
 
-if (casper.cli.args.length < 2) {
+if (casper.cli.args.length < 1) {
   casper
-    .echo("Usage: $ casperjs screenshots.js http://example.com myfile.png")
+    .echo("Usage: $ casperjs screenshots.js http://example.com")
     .exit(1)
   ;
 } else {
@@ -35,32 +18,15 @@ if (casper.cli.args.length < 2) {
 
 casper.start(screenshotUrl, function() {
   this.echo('Current location is ' + this.getCurrentUrl(), 'info');
-});
-
-casper.each(viewports, function(casper, viewport) {
-  this.then(function() {
-    this.viewport(viewport.viewport.width, viewport.viewport.height);
-  });
+  this.viewport(1024, 768)
   this.thenOpen(screenshotUrl, function() {
     this.wait(3000);
   });
-  this.then(function(){
-    this.echo('Screenshot for ' + screenshotFileName, 'info');
-    this.capture(screenshotFileName, {
-        top: 0,
-        left: 0,
-        width: viewport.viewport.width,
-        height: viewport.viewport.height
-    });
+  this.echo('Screenshot for ' + screenshotFileName, 'info');
+  this.capture(screenshotFileName, {
+        top: 0, left: 0, width: 1024, height: 768
   });
 });
 
-casper.run();
 
-function pad(number) {
-  var r = String(number);
-  if ( r.length === 1 ) {
-    r = '0' + r;
-  }
-  return r;
-}
+casper.run();
